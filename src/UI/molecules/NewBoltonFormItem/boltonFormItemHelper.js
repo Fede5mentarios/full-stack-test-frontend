dd33a98fe81145d0415aaeafc6631820a87ec8ac
Federico <fbernst@atixlabs.com>
2019-10-24 19:47:08 -0300

    add redirection and a new rule in the pack creation

--- src/UI/molecules/NewBoltonFormItem/boltonFormItemHelper.js
+++ src/UI/molecules/NewBoltonFormItem/boltonFormItemHelper.js
@@ -1,14 +1,25 @@
 export const convertValuesToBoltonDTOs = values => {
-	return Object.keys(values)
-		.filter(it => it.startsWith("bolton-"))
-		.map(boltonKey => {
-			const key = boltonKey.split("-").pop();
-			return {
-				amount: values[`bolton-${key}`],
-				type: values[`boltonType-${key}`]
-			};
-		});
+	const boltonValues = Object.keys(values).filter(it =>
+		it.startsWith("bolton-")
+	);
+	const boltonsDTO = boltonValues.map(boltonKey => {
+		const key = boltonKey.split("-").pop();
+		return {
+			amount: values[`bolton-${key}`],
+			type: values[`boltonType-${key}`]
+		};
+	});
+	if (moreThanTwoOfTheSameType(boltonsDTO))
+		throw new Error("Sólo puede haber un bono de cada tipo");
+	return boltonsDTO;
 };
+
+const moreThanTwoOfTheSameType = dtos =>
+	dtos.reduce(
+		(hasTwoiceOneType, dto) =>
+			hasTwoiceOneType || dtos.filter(it => it.type === dto.type).length > 1,
+		false
+	);
 
 export const createEmptyBolton = boltons => {
 	const lastBolton = boltons.pop();
